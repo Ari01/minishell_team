f81eded9919ec8b250dd002497cff35079333037
denis <denis.c1@live.fr>
2021-09-10 18:20:40 +0200

    builtins cd

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: xuwang <xuwang@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/09/09 19:48:16 by xuwang            #+#    #+#              #
-#    Updated: 2021/09/10 16:42:12 by xuwang           ###   ########.fr        #
+#    Updated: 2021/09/10 18:20:16 by xuwang           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -21,7 +21,7 @@ builtin/echo.c \
 builtin/cd.c \
 builtin/pwd.c \
 builtin/exit.c \
-builtin/env.c \
+builtin/env.c
 
 
 OBJS			= $(SRCS:.c=.o)
--- builtin/cd.c
+++ builtin/cd.c
@@ -6,7 +6,7 @@
 /*   By: xuwang <xuwang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/09 19:26:15 by xuwang            #+#    #+#             */
-/*   Updated: 2021/09/10 17:56:16 by xuwang           ###   ########.fr       */
+/*   Updated: 2021/09/10 18:16:18 by xuwang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,20 @@ static int check_file(t_cmd *cmd)
     return (1);
 }
 
+void printerror(char *msg1, char *msg2) {
+    ft_putstr_fd(msg1, STDERR_FILENO);
+    ft_putstr_fd(msg2, STDERR_FILENO);
+    ft_putstr_fd("\n", STDERR_FILENO);
+}
+
 int ft_cd(t_cmd *cmd)
 {
     if (cmd && cmd->cmd[1] == NULL)
         return 0;
     else if (cmd && cmd->cmd[2] != NULL)
-        ft_putstr_fd("cd: string not in pwd: %s\n", cmd->cmd[1]);
+        printerror("cd: string not in pwd: ", cmd->cmd[1]);
     else if (cmd && cmd->cmd[1] != NULL && !(check_file(cmd)))
-         printf("cd: no such file or directory: %s\n", cmd->cmd[1]);
+        printerror("cd: no such file or directory: ", cmd->cmd[1]);
     else if (cmd && cmd->cmd[1] && check_file(cmd))
     {
         if (!chdir(cmd->cmd[1]))
