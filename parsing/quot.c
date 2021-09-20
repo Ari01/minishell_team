c7d92df21b0dbd82b6211716c1c52177994b2be1
xueming-wang <wxueming0614@hotmail.com>
2021-09-21 00:48:50 +0200

    quot

--- parsing/quot.c
+++ parsing/quot.c
@@ -6,69 +6,18 @@
 /*   By: xuwang <xuwang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/17 14:34:34 by xuwang            #+#    #+#             */
-/*   Updated: 2021/09/21 00:45:45 by xuwang           ###   ########.fr       */
+/*   Updated: 2021/09/21 00:48:30 by xuwang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// t_quot init_quot(t_quot quot)
-// {
-//     ft_bzero(&quot, sizeof(t_quot));
-//     return (quot);
-// }
-// t_quot get_quot(char *str, t_quot quot)
-// {
-//     int i = 0;
-//     while (str[i])
-//     {
-//         if (str[i] == '"')
-//             quot.doub_quot = 1;
-//         if (str[i] == '\'')
-//             quot.sing_quot = 1;
-//         i++;
-//     }
-//     return (quot);
-// }
-
-// int check_quot_exit(char *s)
-// {
-//     int i = 0;
-     
-//     while (s[i])
-//     {
-//         if (s[i + 1] == '"' || s[i] == '\'')
-//             return (1);        
-//     }
-//     return (0);
-// }
 #define STATUS_OPEN 1
 #define STATUS_CLOSE 0
 #define IS_SQ 1
 #define IS_DQ 2
 #define NO_Q 0
 
-void    quote_open(char *s) {} // quote_status = OPEN ----> #define OPEN 1
-void    quote_close() {} // quote_status = CLOSE ----> #define CLOSE 0
-
-
-    
-    
-    char  *hanling_quot(t_ms *ms, t_quot quot)
-    {
-        char *tmp = ms->rdl;  
-        int i = 0;
-        int j = 0;
-            
-        while(ms->rdl[i] && (ms->rdl[i] != '"' && ms->rdl[i] != '\'')) //first place of quot
-            i++;
-        while(ms->rdl[i] && (ms->rdl[i + 1] != '"' && ms->rdl[i] != '\''))  // i is close quot
-            tmp[j++] = ms->rdl[i++]; 
-           // arg =ft_split(ms->rdl, ' ');  //sepa str par espase like : e"ch"o "a bc" 'ws w''|' "123" | echo "2ee" 
-        
-        
-       return (tmp);
-    }
     
 
 t_quot quote_init(void) {
@@ -80,16 +29,7 @@ t_quot quote_init(void) {
     return quote_info;
 }
 
-// char *check_cmd_quote(char *cmd) {
-//     t_quot quote_info;
 
-
-//     // init
-//     quote_info = quote_init();
-
-
-    
-// }
 
 /**
  * 命令: echo                    "'" "'""test hello" "test2" " " ok
@@ -109,11 +49,6 @@ t_quot quote_init(void) {
  * 2) 然后每个cmds[x]个个处理, 比如:
  * cmds[2]: "'""test hello" ---> test hello
 */
-
-
-
-
-
 
 int cmds_len(char *cmd) {
     int i = 0;
@@ -174,6 +109,13 @@ int main() {
 }
 
 
+char  *hanling_quot(t_ms *ms, t_quot quot)
+{
+       
+        
+        
+       
+}
 
 
 
