9a0ca970875b43bd7c53a1d75e20243495b0052e
denden <denis.c1@live.fr>
2021-09-21 14:10:19 +0200

    redir en cours

--- execution/redirection.c
+++ execution/redirection.c
@@ -6,7 +6,7 @@
 /*   By: dchheang <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/10 17:50:32 by dchheang          #+#    #+#             */
-/*   Updated: 2021/09/20 19:10:04 by dchheang         ###   ########.fr       */
+/*   Updated: 2021/09/21 13:43:54 by dchheang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,31 +42,27 @@ void	redirect_in_out(t_ms *ms, int newfd, int flags)
 void	read_from_current_input(t_ms *ms)
 {
 	int		rd;
-	int		fd;
 	char	buff[BUFFER_SIZE];
+	t_cmd	next_cmd;
 
 	if (ms->cmd_list_ite->next == NULL)
 		print_error_msg("missing argument after redirection\n", SYNTAX_ERR, ms);
 	next_cmd = *(t_cmd *)ms->cmd_list_ite->next->content;
-	fd = open("/tmp/heredoc.txt", O_RDWR | O_APPEND | O_CREAT, 0666);
+	ms->current_input = open("./tmp/heredoc.txt", O_RDWR | O_CREAT, 0666);
 	rd = read(STDIN_FILENO, buff, BUFFER_SIZE - 1);
 	while (ft_strncmp(buff, next_cmd.cmd[0], rd - 1) && rd != -1)
 	{
-		buff[rd] = 0;
-		write(fd, buff, rd);
+		write(ms->current_input, buff, rd);
 		rd = read(STDIN_FILENO, buff, BUFFER_SIZE - 1);
 	}
-	if (rd == -1)
+	if (rd == -1 || dup2(ms->current_input, STDIN_FILENO) == -1)
 		print_error_msg(strerror(errno), READ_WRITE_ERR, ms);
-	close(fd);
-	return (line);
+	close(ms->current_input);
 }
 
 void	redirect(t_ms *ms, t_cmd *current_cmd)
 {
-	t_cmd	*tmp;
 	t_cmd	next_cmd;
-	char	*line;
 
 	if (current_cmd->flag == SLR)
 		redirect_in_out(ms, STDIN_FILENO, O_RDWR);
@@ -75,7 +71,7 @@ void	redirect(t_ms *ms, t_cmd *current_cmd)
 	else if (current_cmd->flag == DRR)
 		redirect_in_out(ms, STDOUT_FILENO, O_RDWR | O_CREAT | O_APPEND);
 	else
-		line = read_from_current_input(ms);
+		read_from_current_input(ms);
 	if (!current_cmd->cmd[0])
 	{
 		printf("cc = %s\n", current_cmd->cmd[0]);
