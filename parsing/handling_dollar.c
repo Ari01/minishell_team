6338654b1f0753015b95ff9b8dbc4e461d864da6
denis <denis.c1@live.fr>
2021-09-30 15:48:29 +0200

    dollar is ok

--- parsing/handling_dollar.c
+++ parsing/handling_dollar.c
@@ -6,130 +6,11 @@
 /*   By: xuwang <xuwang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/27 15:54:05 by xuwang            #+#    #+#             */
-/*   Updated: 2021/09/30 15:17:41 by xuwang           ###   ########.fr       */
+/*   Updated: 2021/09/30 15:47:46 by xuwang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-// //echo "1223 $PATH 2w21$ $USER" | echo $PATH
-// // static int check_name(char *s)   //name must a-z, A-Z, _
-// // {
-// //     int i;
-    
-// //     i = 0;
-// //     while(s[i])
-// //     {
-// //         if (!(s[i] >= 'A' && s[i] <= 'Z') && !(s[i] >= 'a' && s[i] <= 'z') && s[i] != '_')
-// //             return (0);
-// //         i++;
-// //     }
-// //     return (1);
-// // }
-
-// static void  check_exit_add(t_list *_change_, char *dollar, t_list *env_list)   //确认一个list_dollar的值存在与否然后加进去
-// {
-//     int len = ft_strlen(dollar);
-//     char *str = NULL;
-//     char *env = NULL;
-//     int i = 0;
-   
-     
-//     while (env_list)
-//     {
-//         if (ft_strncmp(dollar, (char *)env_list->content, len) == 0 &&
-//         (((char *)env_list->content)[len] == '\0' || ((char *)env_list->content)[len] == '='))
-//         {
-//               str = ft_strdup((char *)env_list->content);
-//               len = ft_strlen(str);
-//               while (str[i] && str[i] != '=')
-//                     i++;
-//               env = ft_strdup(ft_substr(env_list->content, i, (len-i)));
-//               ft_lstadd_back(&_change_, ft_lstnew((void *)env));
-//         }
-//         else
-//         {
-//             ft_lstadd_back(&_change_, ft_lstnew((void *)""));
-//         }
-//         env_list = env_list->next;
-//     }
-// }
-
-
-// static t_list *change_list(t_list *_change_, t_list *dollar_list, t_list *env_list)  //确认所有的list_dollar的值存在与否然后加进去 返回一个需要改变的值
-// {
-    
-//     while (dollar_list)
-//     {
-//         check_exit_add(_change_, (char *)dollar_list->content, env_list);
-//         dollar_list = dollar_list->next;
-//     }
-//     return (_change_);
-// }
-
-// t_list  *get_dollar_env(char *rdl, t_list *dollar_list) //保存$后面的list_dollar  exmple: PATH USER 1 abc 
-// {
-//     char *env = NULL;
-//     int start = 0;
-//     int i = 0;
-//     int len = 0;
-//     while (rdl[i])
-//     {
-//         if (rdl[i] == '$' && rdl[i + 1] != ' ' && rdl[i + 1] != '\0' && rdl[i + 1] != '"'  && rdl[i + i] != '\'' && rdl[i + i] != '$')
-//         {
-//             start = i + 1;
-//             while(rdl[i + len] && rdl[i + len] != ' ' && rdl[i + len] != '\0' && rdl[i + len] != '"'  && rdl[i + len] != '\'' && rdl[i + len] != '$')
-//                 len++;
-//             env = ft_strdup(ft_substr(rdl, start, (len-1)));
-//             ft_lstadd_back(&dollar_list, ft_lstnew((void*)env));
-//         }
-//         start = 0;
-//         len = 0;
-//         i++;       
-//     }
-//     return (dollar_list);
-// }
-
-
-// char  *hanlding_dollar(char *rdl, t_list *env_list)  //创建新的rdl
-// {
-//     int i = 0;
-//     t_list *dollar_list = NULL;
-//     dollar_list = get_dollar_env(rdl, dollar_list);
-//     t_list *_change_ = NULL;
-//     _change_ =  change_list(_change_, dollar_list, env_list);
-//     char *new_rdl = ft_strdup("");
-//   while (rdl[i])
-//   {
-//       if(rdl[i] == '$' && rdl[i + 1] != ' ' && rdl[i + i] != '\0' && rdl[i + i] != '"'  && rdl[i + i] != '\'' && rdl[i + i] != '$')
-//       {
-//           while (_change_)
-//           {
-//               new_rdl = ft_join(new_rdl, (char *)_change_->content);
-//               //_change_ = _change_->next;
-//           }
-//           while (rdl[i] != ' ' || rdl[i] != '\0' || rdl[i] != '"' || rdl[i] != '\'' || rdl[i] != '$')
-//                 i++;
-//       }
-//     //   else 
-//     //   {
-//     //       new_rdl = ft_join(new_rdl, &rdl[i]);
-//     //   }
-//       i++;
-//   }
-//     return (new_rdl);    
-// }
-// int main()
-// {
-//     t_list *new;
-//     char *rdl = " echo \"1223 $PATH $1 $ $USER\" | echo $PATH ";
-//      new =  get_dollar_env(rdl);
-//      while(new)
-//      {
-//          printf("%s\n", (char *)new->content);
-//          new = new->next;
-//      }     
-//     return (0);
-// }
 
 int check_dollar(char *cmd)
 {
@@ -189,7 +70,7 @@ char  *hanlding_dollar(char *cmd, t_list *env_list)  //创建新的rdl
     int len = 0;
     char *dollar = NULL;
     char *new_cmd = NULL;
-    // char *c = NULL;
+    char *c = NULL;
     while (cmd[i])
     {
         if(cmd[i] == '$' && cmd[i + 1] != ' ' && cmd[i + 1] != '\0')
@@ -198,7 +79,14 @@ char  *hanlding_dollar(char *cmd, t_list *env_list)  //创建新的rdl
             if (cmd[i] >= '0' && cmd[i] <= '9')
             {
                 new_cmd = ft_join(new_cmd, "");
-                //printf("new_cmd: %s\n", new_cmd);
+                i++;
+                if (cmd[i] != '\0' && cmd[i] != ' ')
+                {
+                    start = i;
+                    while (cmd[i] && cmd[i] != '\0' && cmd[i] != ' ')
+                        i++;
+                    new_cmd = ft_join(new_cmd, ft_substr(cmd, start, i - start));
+                }
             }
             else 
             {
@@ -214,22 +102,18 @@ char  *hanlding_dollar(char *cmd, t_list *env_list)  //创建新的rdl
                 }
                 else
                 {
-                    //printf("new_cmd2: %s\n", new_cmd);
                      new_cmd = ft_join(new_cmd, "");
                 }
+                i = i + len;
             }
         }
-
+     
         else
         {
-            // //printf("cmd[i]: %c\n", cmd[i]);
-            // printf("cmd : [%s]\n", cmd);
-            // c = ft_substr(cmd, i, 1);
-            // printf("? : [%s]\n", c);
-            // new_cmd = ft_join(new_cmd, c);
-            // free(c);
-            // c = NULL;
-            //printf("new_cmd3: %s\n", new_cmd);
+            c = ft_substr(cmd, i, 1);
+            new_cmd = ft_join(new_cmd, c);
+            free(c);
+            c = NULL;
         }
         start = 0;
         len = 0;
