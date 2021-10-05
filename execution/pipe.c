67a69962493dd32518bde46ae142aae1befcc003
denden <denis.c1@live.fr>
2021-10-05 19:05:32 +0200

    bug pipes ls | ls | ls

--- execution/pipe.c
+++ execution/pipe.c
@@ -6,7 +6,7 @@
 /*   By: dchheang <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/10 13:12:12 by dchheang          #+#    #+#             */
-/*   Updated: 2021/10/05 18:43:18 by dchheang         ###   ########.fr       */
+/*   Updated: 2021/10/05 19:04:44 by dchheang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,6 @@ void	exec_child(t_ms *ms, int pipe_fd)
 
 	ms->cmd_list_ite = ms->cmd_list_ite->next;
 	cmd = *(t_cmd*)ms->cmd_list_ite->content;
-	if (dup2(ms->fd_out, STDOUT_FILENO) == -1)
-		print_error_msg(strerror(errno), PIPE_ERR, ms);
 	if (cmd.in_streams || cmd.out_streams)
 		redirect(ms, &cmd);
 	if (!cmd.in_stream_head)
@@ -44,7 +42,6 @@ void	exec_parent(t_ms *ms, int pipe_fd)
 			print_error_msg(strerror(errno), PIPE_ERR, ms);
 	}
 	run_cmd(ms, &cmd);
-	reset_fdin_fdout(ms);
 }
 
 void	run_pipe(t_ms *ms)
@@ -69,6 +66,7 @@ void	run_pipe(t_ms *ms)
 		exec_parent(ms, pipe_fd[1]);
 		close(pipe_fd[0]);
 		close(pipe_fd[1]);
+		waitpid(pid, NULL, 0);
+		reset_fdin_fdout(ms);
 	}
-	waitpid(pid, NULL, 0);
 }
